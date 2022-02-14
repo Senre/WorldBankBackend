84c5fd0788307ed51a120f1893992895f97fa221
Senre <kishorshris@gmail.com>
2022-02-14 16:09:33 +0000

    added in get function in server to receive indicator information on a country based on inputs

--- server.js
+++ server.js
@@ -19,4 +19,24 @@ const corsInputs = {
   credentials: true,
 };
 
-app.use(abcCors(corsInputs));
+app.use(abcCors(corsInputs)).get("/:country", showCountryData);
+
+async function showCountryData(server) {
+  const { country } = await server.params;
+  const { indicator, startYear, endYear } = await server.body;
+
+  let query = `SELECT * FROM Indicators WHERE countryName = ?`;
+  const queryFilters = [country];
+
+  if (indicator) {
+    query += ` AND IndicatorName = ?`;
+    queryFilters.push(indicator);
+  }
+  if (startYear) {
+    query += ` AND Year BETWEEN ? AND ?`;
+    queryFilters.push(startYear, endYear);
+  }
+
+  const countryData = [...(await db.query(query, queryFilters).asObjects())];
+  server.json(countryData, 200);
+}
