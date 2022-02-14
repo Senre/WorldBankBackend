936e0b0bdcd94703ffcf94f8980aa77171b365a6
Senre <kishorshris@gmail.com>
2022-02-14 14:53:43 +0000

    made base server

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+import { Application } from "https://deno.land/x/abc@v1.3.3/mod.ts";
+import { DB } from "https://deno.land/x/sqlite@v2.5.0/mod.ts";
+import { abcCors } from "https://deno.land/x/cors/mod.ts";
+import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
 
+const db = new DB("database.sqlite");
+const app = new Application();
+const PORT = 8080;
+
+const corsInputs = {
+  methods: "POST,GET",
+  allowedHeaders: [
+    "Authorization",
+    "Content-Type",
+    "Accept",
+    "Origin",
+    "User-Agent",
+  ],
+  credentials: true,
+};
+
+app.use(abcCors(corsInputs));
