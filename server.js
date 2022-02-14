bc2b1a6707e50f18dbdc95d2b7a06bf50c30234e
tomw13 <tomwaghorn13@gmail.com>
2022-02-14 17:32:08 +0000

    merge thing

--- server.js
+++ server.js
@@ -20,24 +20,54 @@ const corsInputs = {
 };
 
 app.use(abcCors(corsInputs));
+app.get("/:country", showCountryData);
+app.post("/login", checkUserLogin);
 
-app.get('/searchpage' searchPageInfo)
-.post("/login", checkUserLogin);
+async function showCountryData(server) {
+  const { country } = await server.params;
+  const countryDecoded = decodeURIComponent(country);
+  const { indicator, startYear, endYear } = await server.body;
 
-async function getSearchPage(server) {
-    const query = 
-      `SELECT  shortname, currencyunit, region
-            FROM stories
-            LEFT JOIN votes 
-            ON stories.id = votes.story_id
-            GROUP BY stories.id
-            ORDER BY score DESC`
-    const stories = await [
-      ...db
-        .query(
-        
-        )
-        .asObjects()
+  const [countryExists] = [
+    ...(await db.query(`SELECT ShortName FROM Country WHERE ShortName = ?`, [
+      countryDecoded,
+    ])),
+  ];
+  if (countryExists) {
+    let query = `SELECT * FROM Indicators WHERE countryName = ?`;
+    const queryFilters = [countryDecoded];
+
+    if (indicator) {
+      query += ` AND IndicatorName = ?`;
+      queryFilters.push(indicator);
+    }
+    if (startYear) {
+      query += ` AND Year BETWEEN ? AND ?`;
+      queryFilters.push(startYear, endYear);
+    }
+
+    const countryData = [...(await db.query(query, queryFilters).asObjects())];
+    if (countryData) {
+      server.json(countryData, 200);
+    } else {
+      server.json(
+        {
+          statusCode: 204,
+          message: "204: No content. No data found with those restraints",
+        },
+        204
+      );
+    }
+  } else {
+    server.json(
+      {
+        statusCode: 400,
+        message:
+          "400: Bad Request. That country does not exist in our database",
+      },
+      400
+    );
+  }
 }
 
 async function checkUserLogin(server) {
