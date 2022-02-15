153366782e301ff1c36d0b6256fb0042c5abe461
Mariam01010010 <94632221+Mariam01010010@users.noreply.github.com>
2022-02-15 17:11:58 +0000

    setCookies done

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import { Application } from "https://deno.land/x/abc@v1.3.3/mod.ts";
 import { DB } from "https://deno.land/x/sqlite@v2.5.0/mod.ts";
 import { abcCors } from "https://deno.land/x/cors/mod.ts";
 import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
 const db = new DB("database.sqlite");
 const app = new Application();
@@ -20,8 +21,80 @@ const corsInputs = {
 };
 
 app.use(abcCors(corsInputs));
-app.get("/:country", showCountryData);
-app.post("/login", checkUserLogin);
+
+app
+  .get("/:country", showCountryData)
+  .post("/login", checkUserLogin)
+  .post("/sessions", createSession)
+  .post("/register", registerUser)
+
+
+async function registerUser(server) {
+  const { email, password } = await server.body;
+  const salt = await bcrypt.genSalt(8);
+  const passwordEncrypted = await bcrypt.hash(password, salt)
+  let exists =
+    `IF EXISTS (SELECT email FROM wbd-db WHERE email = ?)`,[
+      email
+    ];
+  if (exists) {
+      return server.json({error: "User already exists"}, 400)
+  } else {
+      const query = (`INSERT INTO users (email, password, salt, created_at, updated_at) VALUES (?, ?, ?, datetime('now'), datetime('now'))`,[email, passwordEncrypted, salt])
+      await db.query(query)
+      return server.json({success: "User registered successfully."}, 200)
+  }    
+
+async function createSession(server) {
+  const sessionId = v4.generate();
+  await db.query(
+    `INSERT INTO sessions (uuid, user_id, created_at) VALUES (?, ?, datetime('now'))`,
+    [sessionId, user_id]
+  );
+
+  const user = [
+    ...(
+      await db.query("SELECT * FROM users WHERE id = ?", [user_id])
+    ).asObjects(),
+  ][0];
+
+  const expiryDate = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000);
+  server.setCookie({
+    name: "sessionId",
+    value: sessionId,
+    expires: expiryDate,
+    path: "/home",
+  });
+  server.setCookie({
+    name: "user_id",
+    value: user_id,
+    expires: expiryDate,
+    path: "/home",
+  });
+  server.setCookie({
+    name: "email",
+    value: user.email,
+    expires: expiryDate,
+    path: "/home",
+  });
+}
+
+async function getCurrentUser(sessionId) {
+  const [session] = await [
+    ...db
+      .query(
+        `SELECT * FROM sessions WHERE JULIANDAY(datetime('now')) - JULIANDAY(created_at) < 7 AND uuid = ?`,
+        [sessionId]
+      )
+      .asObjects()
+  ]
+  const [user] = await [
+    ...db
+      .query('SELECT * FROM users WHERE id = ?', [session.user_id])
+      .asObjects()
+  ]
+  return user
+}
 
 async function showCountryData(server) {
   const { country } = await server.params;
@@ -73,7 +146,7 @@ async function showCountryData(server) {
 async function checkUserLogin(server) {
   const { email, password } = await server.body;
   let exists =
-    `IF EXISTS (SELECT email, password FROM wbd-db WHERE AND email = ? AND password = ?)`[
+    `IF EXISTS (SELECT email, password FROM wbd-db WHERE email = ? AND password = ?)`[
       (email, password)
     ];
   if (exists) {
