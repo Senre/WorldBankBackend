95a1e1489f3e90a0199de041601e5e22daff241d
Senre <kishorshris@gmail.com>
2022-02-15 17:09:16 +0000

    will have to see

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.post("/login", checkUserLogin);
 app.start({ port: PORT });
 
 async function showCountryData(server) {
+  console.log(await server);
   const { country } = await server.params;
   const countryDecoded = decodeURIComponent(country);
 
