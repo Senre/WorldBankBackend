e9e91a3345e5b436f38b19781a7ffab609699b84
tomw13 <tomwaghorn13@gmail.com>
2022-02-17 13:21:59 +0000

    get user searches

--- seed.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO users (email, password, salt, created_at, updated_at) VALUES ("pleasework@gmail.com", "$2a$08$T6eYq2UycSXk34ppoBe7wOPbKxm300K0Fpin7gC6a4bvnZegBZ44i", "$2a$08$T6eYq2UycSXk34ppoBe7wO", datetime('now'), datetime('now'))
